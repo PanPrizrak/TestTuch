9e14ae5857422fcbfdd49e45e3bbe41f3a87c35a
PanPrizrak <asirozh@gmail.com>
2018-05-18 20:27:38 +0300

    add test

--- TuchSoft/src/Koch.java
+++ TuchSoft/src/Koch.java
@@ -1,45 +1,52 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Koch {
     public static void main(String[] args) {
         System.out.println("Resultat dlya massiva {0, 1, 3, 5, 6, 8, 12, 17} " + Shet(new int[]{0, 1, 3, 5, 6, 8, 12, 17}));
+        System.out.println("Resultat dlya massiva {0, 1, 2, 3, 4, 8, 9, 11} " + Shet(new int[]{0, 1, 2, 3, 4, 8, 9, 11}));
     }
 
     public static String Shet(int[] mas) {
-        System.out.printf(new String(String.valueOf(mas.equals(mas[2]))));
+
         int k = 1;
-        Positions position = new Positions();
-        ArrayList<Positions> listPos = new ArrayList<Positions>();
-        String result = "false";
-        position.setPisitions(0, 1);
-        listPos.add(position);
+
+        ArrayList<Integer> masList = new ArrayList<>();
+
+        for(int b: mas) {
+            masList.add(new Integer(b));
+        }
+
+        ArrayList<Positions> posList = new ArrayList<>();
+        posList.add(new Positions(0, k));
 
         for (int i = 0; i < mas.length - 1; i++) {
 
-            for (int j = 0; j < listPos.size(); j++ ) {//for (int j = listPos.get(listPos.indexOf(new Positions(i, k))).getPosition(); j <= listPos.get(listPos.lastIndexOf(new Positions(i, k))).getPosition(); j++) {
-                if(i == listPos.get(j).getPosition()) {
-                    if (mas.equals(mas[j] + k)) {
-                        listPos.add(new Positions(i+1,k));
+            for (int j = 0; j < posList.size(); j++ ) {
+
+                if(i == posList.get(j).getPosition()) {
+
+                    int bufPos = posList.get(j).getPosition();
+                    int bufK =  posList.get(j).getK();
+
+                    if (masList.contains(mas[bufPos] + bufK)) {
+                        posList.add(new Positions(masList.indexOf(mas[bufPos]+bufK),bufK));
                     }
 
-                    if (mas.equals(mas[j] + k + 1)) {
-                        k += 1;
-                        position.setPisitions(i + 1, k);
-                        listPos.add(position);
+                    if (masList.contains(mas[bufPos] + bufK + 1)) {
+                        posList.add(new Positions(masList.indexOf(mas[bufPos] + bufK + 1),bufK+1));
                     }
 
-                    if (k > 1 && mas.equals(mas[j] + k - 1)) {
-                        k -= 1;
-                        position.setPisitions(i - 1, k);
-                        listPos.add(position);
+                    if (k > 1 && masList.contains(mas[bufPos] + bufK - 1)) {
+                        posList.add(new Positions(masList.indexOf(mas[bufPos] + bufK - 1),bufK - 1));
                     }
                 }
 
             }//for j
         }
 
-        if (listPos.get(listPos.size()-1).getPosition() == mas[mas.length-1])
-            result = "true";
-        return result;
+        if (posList.get(posList.size()-1).getPosition() == mas.length-1)
+            return "true";
+        return "false";
     }
 }
